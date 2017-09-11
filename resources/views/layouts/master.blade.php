0b70e9f8c40a5f317cf1aa52996fe678e0d405b3
phuc.nguyen <phuc.nguyen@staff.distance-c.asia>
2017-09-11 16:35:02 +0700

    Commit 11-09-2017 16:34:38

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -21,12 +21,173 @@
     <![endif]-->
     {{--<script src="{{ asset('/js/app.js') }}"></script>--}}
     <link rel="icon" href="{{ url('images/logo.jpg') }}" sizes="32x32" />
+    <style>
+        .ct-topbar {
+            text-align: right;
+            background: #eee;
+        }
+        .ct-topbar__list {
+            margin-bottom: 0px;
+        }
+        .ct-language__dropdown{
+            padding-top: 8px;
+            max-height: 0;
+            overflow: hidden;
+            position: absolute;
+            top: 110%;
+            left: -3px;
+            -webkit-transition: all 0.25s ease-in-out;
+            transition: all 0.25s ease-in-out;
+            width: 100px;
+            text-align: center;
+            padding-top: 0;
+            z-index:200;
+        }
+        .ct-language__dropdown li{
+            background: #222;
+            padding: 5px;
+        }
+        .ct-language__dropdown li a{
+            display: block;
+        }
+        .ct-language__dropdown li:first-child{
+            padding-top: 10px;
+            border-radius: 3px 3px 0 0;
+        }
+        .ct-language__dropdown li:last-child{
+            padding-bottom: 10px;
+            border-radius: 0 0 3px 3px;
+        }
+        .ct-language__dropdown li:hover{
+            background: #444;
+        }
+        .ct-language__dropdown:before{
+            content: '';
+            position: absolute;
+            top: 0;
+            left: 0;
+            right: 0;
+            margin: auto;
+            width: 8px;
+            height: 0;
+            border: 0 solid transparent;
+            border-right-width: 8px;
+            border-left-width: 8px;
+            border-bottom: 8px solid #222;
+        }
+        .ct-language{
+            position: relative;
+            background: #00aced;
+            color: #fff;
+            padding: 10px 0;
+        }
+        .ct-language:hover .ct-language__dropdown{
+            max-height: 200px;
+            padding-top: 8px;
+        }
+        .list-unstyled {
+            padding-left: 0;
+            list-style: none;
+        }
+
+        .navbar-brand>img {
+            padding-top: 11px;
+            width: 130px;
+            margin-left: 15px;
+        }
+        .navbar-brand {
+            height: auto;
+            margin: 0;
+            padding: 0;
+            margin-right: 20px;
+        }
+        .navbar {
+            background-color: #000000;
+        }
+        .navbar-default .navbar-nav > .active > a {
+            padding: 8px 19px 9px !important;
+        }
+        .navbar-nav > li.active {
+            padding: 8px 0px 9px 0;
+        }
+        .navbar-right {
+            padding-top: 0;
+        }
+        .navbar-default .navbar-nav > li > a::after {
+            background-color: transparent;
+            border-bottom: 3px solid #d2282e;
+        }
+        .navbar-default .navbar-nav>li {
+            display: inline-block;
+            text-align: center;
+            float: none;
+        }
+        .navbar-default .navbar-nav>li>a {
+            color: #fff;
+        }
+        .navbar-default .navbar-nav>li>a:hover {
+            color: #fff;
+            background-color: #0392CC;
+        }
+    </style>
 </head>
 
 <body class="home page-template page-template-page-blank page-template-page-blank-php page page-id-4 header-shadow lightbox nav-dropdown-has-arrow">
 {{--<a class="skip-link screen-reader-text" href="#main">Skip to content</a>--}}
 
 <div id="wrapper">
+
+    <div class="ct-topbar">
+        <div class="container">
+            <ul class="list-unstyled list-inline ct-topbar__list">
+                <li class="ct-language">Language <i class="fa fa-arrow-down"></i>
+                    <ul class="list-unstyled ct-language__dropdown">
+                        <li><a href="#googtrans(en|en)" class="lang-en lang-select" data-lang="en"><img src="{{ url('images/flag-eng.png') }}" width="25" height="25" alt="USA"></a></li>
+                        <li><a href="#googtrans(en|vi)" class="lang-es lang-select" data-lang="vi"><img src="https://www.solodev.com/assets/google-translate/flag-vietnam.png" alt="VIETNAM"></a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+
+    <script type="text/javascript">
+        function googleTranslateElementInit() {
+            new google.translate.TranslateElement({pageLanguage: 'vi', layout: google.translate.TranslateElement.FloatPosition.TOP_LEFT}, 'google_translate_element');
+        }
+
+        function triggerHtmlEvent(element, eventName) {
+            var event;
+            if (document.createEvent) {
+                event = document.createEvent('HTMLEvents');
+                event.initEvent(eventName, true, true);
+                element.dispatchEvent(event);
+            } else {
+                event = document.createEventObject();
+                event.eventType = eventName;
+                element.fireEvent('on' + event.eventType, event);
+            }
+        }
+
+        jQuery('.lang-select').click(function() {
+            var theLang = jQuery(this).attr('data-lang');
+            jQuery('.goog-te-combo').val(theLang);
+
+            //alert(jQuery(this).attr('href'));
+            window.location = jQuery(this).attr('href');
+            location.reload();
+
+        });
+    </script>
+    <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+
+
+
+
+
+    <div>
+        Tên tôi là Nguyễn Văn Test. Tôi 17 tuổi.
+    </div>
 
     @include('user.header')
 
